03081013ca24037bdf7ef20cb456dd7f319b10ad
vyanni <vianneysusanto@gmail.com>
2026-04-05 22:33:56 -0400

    Adapting to wrapped map

--- edumap-frontend/src/PlanningPage/PlanningPage.tsx
+++ edumap-frontend/src/PlanningPage/PlanningPage.tsx
@@ -1,13 +1,13 @@
 import { NavLink } from "react-router-dom";
 import CourseSelector from "./CourseSelector";
-import NodeCourseMap from "./NodeCourseMap";
+import WrappedMap from "./NodeCourseMap";
 
 function PlanningPage() {
   return (
     <div className={`w-auto h-screen justify-center relative`}>
 
       <div className={`w-full h-full`}>
-        <NodeCourseMap/>
+        <WrappedMap/>
       </div>
       
        <CourseSelector/>
