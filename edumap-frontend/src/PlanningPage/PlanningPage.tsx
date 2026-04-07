16cfa96a9d13eb7e8fb73842aaa035d92608d7f8
vyanni <vianneysusanto@gmail.com>
2026-04-07 13:13:48 -0400

    Having the planning page have leftinfo panel

--- edumap-frontend/src/PlanningPage/PlanningPage.tsx
+++ edumap-frontend/src/PlanningPage/PlanningPage.tsx
@@ -1,13 +1,17 @@
 import { NavLink } from "react-router-dom";
 import CourseSelector from "./CourseSelector";
 import WrappedMap from "./NodeCourseMap";
+import LeftInfoPanel from "./LeftInfoPanel";
 
 function PlanningPage() {
   return (
     <div className={`w-auto h-screen justify-center relative`}>
 
       <div className={`w-full h-full`}>
-        <WrappedMap/>
+        <LeftInfoPanel/>
+        <div className={`w-4/5 h-full right-0 absolute`}>
+          <WrappedMap/>
+        </div>
       </div>
       
        <CourseSelector/>
