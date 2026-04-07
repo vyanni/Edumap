a7573b16fcde71fd1a2abfe6fee4904f709cdd98
vyanni <vianneysusanto@gmail.com>
2026-04-07 13:12:16 -0400

    Add the entire prereq logic, make auto arrows

--- edumap-frontend/src/Hooks/usePreReqLogic.tsx
+++ edumap-frontend/src/Hooks/usePreReqLogic.tsx
@@ -1,3 +1,46 @@
-function usePreReqLogic(){
-    return ()
+import { useEffect } from "react"
+import {type Node, type Edge, MarkerType} from '@xyflow/react'
+
+export function usePreReqLogic(allNodes: Node[], allEdges: Edge[], setEdges: React.Dispatch<React.SetStateAction<Edge[]>>){
+    useEffect(() => {
+        const newEdges = [...allEdges];
+        let newPreReq = false;
+
+        const currentNodes = new Set(allNodes.map((node) => node.id));
+        const currentEdges = new Set(allEdges.map((edge) => `${edge.source}->${edge.target}`));
+
+        allNodes.forEach((node) => {
+            const prerequisites = node.data?.prerequisites || [];
+
+            prerequisites.forEach((preReqID: string) => {
+                if(currentNodes.has(preReqID) && !currentEdges.has(`${preReqID}->${node.id}`)){
+                    newEdges.push({
+                        id: `${preReqID}-${node.id}`,
+                        source: preReqID,
+                        target: node.id,
+                        animated: true,
+
+                        style: { 
+                        stroke: '#3b82f6',
+                        strokeWidth: 2,
+                        opacity: 0.6 
+                        },
+                        markerEnd: {
+                            type: MarkerType.ArrowClosed,
+                            width: 20,
+                            height: 20,
+                            color: '#3b82f6',
+                        },
+                    });
+
+                    newPreReq = true;
+                    currentEdges.add(`${preReqID}->${node.id}`);
+                }
+            });
+        });
+
+        if(newPreReq){
+            setEdges(newEdges);
+        }
+    }, [allNodes])
 }
