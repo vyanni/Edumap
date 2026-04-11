f4a89fff4ee1006c97019952baed0af875036467
vyanni <vianneysusanto@gmail.com>
2026-04-11 17:35:35 -0400

    Populated the planning page a lot more

--- edumap-frontend/src/PlanningPage/PlanningPage.tsx
+++ edumap-frontend/src/PlanningPage/PlanningPage.tsx
@@ -1,21 +1,33 @@
 import { NavLink } from "react-router-dom";
 import CourseSelector from "./CourseSelector";
 import WrappedMap from "./NodeCourseMap";
-import LeftInfoPanel from "./LeftInfoPanel";
+import LeftInfoPanel from "./InfoPanel/LeftInfoPanel";
 
 import courseData from '../PlanningPage/CourseNodes.json'
 import RightDescriptionPanel from "./RightDescriptionPanel";
+import { useState } from "react";
+import useSlotsLogic from "../Hooks/useSlotsLogic";
+import { type Node } from "@xyflow/react";
 
 function PlanningPage() {
+  const [selectedNode, setSelectedNode] = useState<Node | null>(null);
+  const {allNodes, nodeTypes, setNodes, onChangeNodes} = useSlotsLogic();
+
   return (
     <div className={`w-auto h-screen justify-center relative`}>
 
       <div className={`w-full h-full`}>
-        <LeftInfoPanel newCourses={courseData}/>
+        <LeftInfoPanel newCourses={courseData} allNodes={allNodes}/>
         <div className={`w-4/5 h-full right-0 absolute`}>
-          <WrappedMap/>
+          <WrappedMap 
+            allNodes={allNodes} 
+            setNodes={setNodes} 
+            onChangeNodes={onChangeNodes} 
+            nodeTypes={nodeTypes}
+            setSelectedNode={setSelectedNode}
+          />
         </div>
-        {/* <RightDescriptionPanel courseData={courseData}/> */}
+        <RightDescriptionPanel selectedNode={selectedNode}/>
       </div>
       
        <CourseSelector/>
