61b1ce2a1b31aed332d5edffebc9d665a1187b53
vyanni <vianneysusanto@gmail.com>
2026-04-10 23:06:52 -0400

    Adding more to the planning page

--- edumap-frontend/src/PlanningPage/PlanningPage.tsx
+++ edumap-frontend/src/PlanningPage/PlanningPage.tsx
@@ -3,15 +3,19 @@ import CourseSelector from "./CourseSelector";
 import WrappedMap from "./NodeCourseMap";
 import LeftInfoPanel from "./LeftInfoPanel";
 
+import courseData from '../PlanningPage/CourseNodes.json'
+import RightDescriptionPanel from "./RightDescriptionPanel";
+
 function PlanningPage() {
   return (
     <div className={`w-auto h-screen justify-center relative`}>
 
       <div className={`w-full h-full`}>
-        <LeftInfoPanel/>
+        <LeftInfoPanel newCourses={courseData}/>
         <div className={`w-4/5 h-full right-0 absolute`}>
           <WrappedMap/>
         </div>
+        {/* <RightDescriptionPanel courseData={courseData}/> */}
       </div>
       
        <CourseSelector/>
