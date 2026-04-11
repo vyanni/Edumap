b5c5c594c91ba0a02b742b27170b06c7a043994c
vyanni <vianneysusanto@gmail.com>
2026-04-10 20:58:58 -0400

    Starting the right description panel

--- edumap-frontend/src/PlanningPage/RightDescriptionPanel.tsx
+++ edumap-frontend/src/PlanningPage/RightDescriptionPanel.tsx
@@ -1,5 +1,9 @@
-function RightDescriptionPanel({courseData}: CourseInfo){
-    return (  );
+function RightDescriptionPanel({courseData}: any){
+    return(
+        <div className={`absolute w-1/5 h-full bg-white dark:bg-black rounded-xl border-4 border-zinc-200 z-45 flex flex-col items-center gap-3`}>
+            <h2 className={`text-bold text-lg p-4 pt-16 text-black`}> Search your Courses</h2>
+        </div>
+    )
 }
 
 export default RightDescriptionPanel
