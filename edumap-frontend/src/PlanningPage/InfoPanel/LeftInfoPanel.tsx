e2f37386a69397c4527f9080cd79b22fe53fedff
vyanni <vianneysusanto@gmail.com>
2026-04-12 20:12:36 -0400

    Moving logic to their respective components

--- edumap-frontend/src/PlanningPage/InfoPanel/LeftInfoPanel.tsx
+++ edumap-frontend/src/PlanningPage/InfoPanel/LeftInfoPanel.tsx
@@ -1,4 +1,4 @@
-import { useMemo, useState } from "react";
+import {useState } from "react";
 import majorData from '../Major&Programs.json';
 import MajorRequirementsPanel from "./MajorRequirementsPanel";
 import SearchCoursesPanel from "./SearchCoursesPanel";
@@ -12,33 +12,6 @@ function LeftInfoPanel({newCourses, allNodes}: any){
         event.dataTransfer.setData('application/reactflow', JSON.stringify(courseData));
         event.dataTransfer.effectAllowed = 'move';
     };
-
-    const activeMajor = majorData?.[0];
-
-    const missingCourses = useMemo(() => {
-        if (!activeMajor) return [];
-
-        const placedCourseIds = new Set(
-            allNodes
-                .filter(node => node.type === 'course')
-                .map(node => node.data?.originalId)
-        );
-
-        return newCourses.filter((course: any) => {
-            const isRequired = activeMajor.requiredCourses?.includes(course.id);
-            const isPlaced = placedCourseIds.has(course.id);
-            
-            return isRequired && !isPlaced;
-        });
-    }, [newCourses, allNodes, activeMajor]);
-
-    const currentCredits = useMemo(() => {
-        return allNodes
-            .filter((node: any) => node.type === 'course')
-            .reduce((total: number, node: any) => {
-                return total + (Number(node.data?.credits) || 0); 
-            }, 0);
-    }, [allNodes]);
 
     return(
         <div className={`absolute w-1/5 h-full bg-white dark:bg-black rounded-xl border-4 border-zinc-200 z-45 flex flex-col items-center gap-3`}>
@@ -62,9 +35,9 @@ function LeftInfoPanel({newCourses, allNodes}: any){
 
             {activeView === 'majorRequirements' && 
             <MajorRequirementsPanel 
-                currentCredits={currentCredits}
-                activeMajor={activeMajor}
-                missingCourses={missingCourses}
+                majorData={majorData}
+                allNodes={allNodes}
+                newCourses={newCourses}
                 onDragStart={onDragStart}
             />}
 
