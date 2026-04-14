146f1660c3b3e066c1684759a352cdc3728d1d40
vyanni <vianneysusanto@gmail.com>
2026-04-13 21:42:57 -0400

    Updated course controller

--- edumap-backend/src/controllers/courseController.ts
+++ edumap-backend/src/controllers/courseController.ts
@@ -7,48 +7,41 @@ export const getAllCourses = async (req: Request, res: Response) => {
             .from('courses')
             .select('*');
 
-        if(error){
-            console.error("DB Error:", error);
-            return res.status(400).json({ error: error.message });
-        }
+        if (error) return res.status(400).json({ error: error.message });
         res.status(200).json(data);
-
-    } catch (error) {
-        console.error("Server Error:", error);
-        res.status(500).json({ message: "Internal server error" });
-    }
+    } 
+    
+    catch (error) { res.status(500).json({ message: "Error loading courses." });}
 };
 
 export const getCourseByID = async (req: Request, res: Response) => {
     try {
         const courseID = req.params.id;
-        const { data, error } = await supabase
-        .from('courses')
-        .select('*')
-        .eq('id', courseID)
-        .single();
+            const { data, error } = await supabase
+            .from('courses')
+            .select('*')
+            .eq('id', courseID)
+            .single();
 
-        if(error){res.status(400).json({error: error.message});}
+        if(error)res.status(400).json({error: error.message});
         res.status(200).json({data});
-
-    } catch (error) {
-        res.status(500).json({ message: "Error fetching course." });
-    }
+    } 
+    
+    catch (error) { res.status(500).json({ message: "Error fetching course." }); }
 };
 
 export const getElectives = async(req: Request, res: Response) => {
     try{
-        const courseList = req.params.list;
+        const courseList = req.params.electiveList;
 
         const { data, error } = await supabase
-        .from('courses')
-        .select('*')
-        .eq('list', courseList);
+            .from('courses')
+            .select('*')
+            .eq('list', courseList);
 
-        if(error){res.status(400).json({error: error.message});}
+        if(error) res.status(400).json({error: error.message});
         res.status(200).json({data});
-
-    } catch(error){
-        res.status(500).json({ message: "Error fetching course." });
-    }
+    } 
+    
+    catch(error){ res.status(500).json({ message: "Error fetching course list." }); }
 }
