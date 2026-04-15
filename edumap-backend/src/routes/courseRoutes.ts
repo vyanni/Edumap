72fa250720406a262d40c16c63e6622efcc7610b
vyanni <vianneysusanto@gmail.com>
2026-04-14 22:43:49 -0400

    Creating the routes for courses

--- edumap-backend/src/routes/courseRoutes.ts
+++ edumap-backend/src/routes/courseRoutes.ts
@@ -1,27 +1,20 @@
 import { Router } from 'express';
-import { getCourses, getCourseById } from '../controllers/courseController';
+import { createCourse, getAllCourses, getCourseByID, updateCourse} from '../controllers/courseController.js';
 
 /**
  * Course Routes:
- * 
  * The Course Routes routes and controller files are ONLY meant for retrieving specific courses
  * These routes are defined as:
- * (getElective): Courses which are part of specific elective lists (CSE-A, ECE-TE, SOCIAL-SCIENCES, etc)
- * (getAllCourses): All courses in general for user searching
- * (getCourseByID): One specific course by its ID/Course code
- *  
- * 
+ * (getAllCourses): All courses in general for user searching | GET Request OR a Query for lists Courses which are part of specific elective lists (CSE-A, ECE-TE, SOCIAL-SCIENCES, etc) GET Request
+ * (getCourseByID): One specific course by its ID/Course code | GET Request
  */
 
 const router = Router();
 
-// When a GET request hits the base path (defined in server.ts), run getCourses
-router.get('/', getCourses);
-
-// When a GET request hits /:id (e.g., /SYDE-101), run getCourseById
-router.get('/:id', getCourseById);
-
-// You would add POST, PUT, DELETE here later
-// router.post('/', createCourse);
+router.get('/', getAllCourses);        
+// ?list= ?fields=
+router.get('/:id', getCourseByID);
+router.post('/create', createCourse);
+router.put('/:id', updateCourse);
 
 export default router;
