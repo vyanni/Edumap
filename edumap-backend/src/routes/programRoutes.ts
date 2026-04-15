c1d54c67bfaa4c5298b655dd443eb77a3051383c
vyanni <vianneysusanto@gmail.com>
2026-04-14 23:16:02 -0400

    Adding routes for programs

--- edumap-backend/src/routes/programRoutes.ts
+++ edumap-backend/src/routes/programRoutes.ts
@@ -1,23 +1,16 @@
 import { Router } from 'express';
-import { getAllPrograms } from '../controllers/programController.ts';
-
-/**
- * Course Routes:
- * 
- * The Course Routes routes and controller files are ONLY meant for retrieving specific courses
- * These routes are defined as:
- * (getElectives): Courses which are part of specific elective lists (CSE-A, ECE-TE, SOCIAL-SCIENCES, etc) GET Request
- * (getAllCourses): All courses in general for user searching | GET Request
- * (getCourseByID): One specific course by its ID/Course code | GET Request
- */
+import {
+    getAllPrograms,
+    getProgramByID,
+    createProgram,
+    updateProgram
+} from '../controllers/programController.js';
 
 const router = Router();
 
-// When a GET request hits the base path (defined in server.ts), run getCourses
 router.get('/', getAllPrograms);
-// router.get('/:id', getCourseByID);
-// router.get('/:electives', getElectives);
-//router.post('/:create', createCourse);
-//router.put('/update', updateCourse);
+router.get('/:id', getProgramByID);
+router.post('/create', createProgram);
+router.put('/:id', updateProgram);
 
 export default router;
