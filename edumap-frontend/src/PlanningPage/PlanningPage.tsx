fe5088171178844c3809454c253ebba35dca4dae
vyanni <vianneysusanto@gmail.com>
2026-04-15 21:57:03 -0400

    Adding more calls to the planning page

--- edumap-frontend/src/PlanningPage/PlanningPage.tsx
+++ edumap-frontend/src/PlanningPage/PlanningPage.tsx
@@ -1,36 +1,67 @@
-import { NavLink } from "react-router-dom";
-import CourseSelector from "./CourseSelector";
 import WrappedMap from "./NodeCourseMap";
 import LeftInfoPanel from "./InfoPanel/LeftInfoPanel";
-
-import courseData from '../PlanningPage/CourseNodes.json'
 import RightDescriptionPanel from "./RightDescriptionPanel";
-import { useState } from "react";
+import { useEffect, useState} from "react";
 import useSlotsLogic from "../Hooks/useSlotsLogic";
 import { type Node } from "@xyflow/react";
+import { useGetNewProgram, useGetPrograms } from "../Hooks/useDBCalls";
+import { supabase } from "../Authentication/supabaseClient";
 
 function PlanningPage() {
+  useEffect(() => {
+    const checkUser = async () => {
+      const { data: { user } } = await supabase.auth.getUser();
+      setUser(user);
+    };
+    checkUser();
+  }, []);
+
   const [selectedNode, setSelectedNode] = useState<Node | null>(null);
-  const {allNodes, nodeTypes, setNodes, onChangeNodes} = useSlotsLogic();
+  const {allNodes, nodeTypes, setNodes, onChangeNodes, allCourses} = useSlotsLogic(user?.id);
+
+  const [allPrograms, setAllPrograms] = useState<any[]>([]);
+  const [selectedLabel, setSelectedLabel] = useState<string | null>(null);
+  const [selectedProgram, setSelectedProgram] = useState<any | null>(null);
+
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [isProgramLoading, setIsProgramLoading] = useState<boolean>(false);
+
+  const [user, setUser] = useState<any>(null);
+
+  useGetPrograms({setAllPrograms, setIsLoading});
+  useGetNewProgram({
+    selectedProgramLabel: selectedLabel,
+    setActiveProgram: setSelectedProgram, 
+    setIsLoading: setIsProgramLoading
+  });
 
   return (
     <div className={`w-auto h-screen justify-center relative`}>
 
       <div className={`w-full h-full`}>
-        <LeftInfoPanel newCourses={courseData} allNodes={allNodes}/>
-        <div className={`w-4/5 h-full right-0 absolute`}>
+        <LeftInfoPanel 
+          newCourses={allCourses} 
+          allNodes={allNodes} 
+          activeMajor={selectedProgram}
+          allPrograms={allPrograms}
+          selectedLabel={selectedLabel}
+          setSelectedLabel={setSelectedLabel}
+          isLoading={isLoading}
+        />
+
+        <div className={`w-3/4 h-full right-0 absolute`}>
           <WrappedMap 
+            userId={user?.id}
             allNodes={allNodes} 
             setNodes={setNodes} 
             onChangeNodes={onChangeNodes} 
             nodeTypes={nodeTypes}
             setSelectedNode={setSelectedNode}
+            isLoading={isLoading}
           />
         </div>
         <RightDescriptionPanel selectedNode={selectedNode}/>
       </div>
-      
-       <CourseSelector/>
     </div>
   );
 };
