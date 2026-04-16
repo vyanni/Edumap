8f519747e3642b4f8c69d836b31ab7f0c91697c4
vyanni <vianneysusanto@gmail.com>
2026-04-15 21:56:44 -0400

    Adding a new view to the left panel

--- edumap-frontend/src/PlanningPage/InfoPanel/LeftInfoPanel.tsx
+++ edumap-frontend/src/PlanningPage/InfoPanel/LeftInfoPanel.tsx
@@ -1,11 +1,11 @@
 import {useState } from "react";
-import majorData from '../Major&Programs.json';
-import MajorRequirementsPanel from "./MajorRequirementsPanel";
+import MajorRequirementsPanel from "./ProgramRequirementsPanel";
 import SearchCoursesPanel from "./SearchCoursesPanel";
+import DegreeCreatorPanel from "./DegreeCreatorPanel";
 
-type PanelType = 'courseSearch' | 'majorRequirements';
+type PanelType = 'courseSearch' | 'majorRequirements' | 'degreePanel';
 
-function LeftInfoPanel({newCourses, allNodes}: any){
+function LeftInfoPanel({newCourses, allNodes, activeMajor, allPrograms, selectedLabel, setSelectedLabel, isLoading}: any){
     const [activeView, setActiveView] = useState<PanelType>('courseSearch');
 
     const onDragStart = (event: React.DragEvent, courseData: any) =>{
@@ -14,17 +14,22 @@ function LeftInfoPanel({newCourses, allNodes}: any){
     };
 
     return(
-        <div className={`absolute w-1/5 h-full bg-white dark:bg-black rounded-xl border-4 border-zinc-200 z-45 flex flex-col items-center gap-3`}>
+        <div className={`absolute w-1/4 h-full bg-white dark:bg-black rounded-xl border-4 border-zinc-200 z-45 flex flex-col items-center gap-3`}>
             <div className={`flex border-b z-50 w-full `}>
                 <button
                     onClick={() => setActiveView('courseSearch')}
-                    className={`flex-1 py-2 text-sm font-medium ${activeView === 'courseSearch' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}
+                    className={`flex-1 py-2 text-xs font-medium ${activeView === 'courseSearch' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}
                 >Search</button>
 
                 <button
+                    onClick={() => setActiveView('degreePanel')}
+                    className={`flex-1 py-2 text-xs font-medium ${activeView === 'degreePanel' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}
+                >Degree</button>
+
+                <button
                     onClick={() => setActiveView('majorRequirements')}
-                    className={`flex-1 py-2 text-sm font-medium ${activeView === 'majorRequirements' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}
-                >Major</button>
+                    className={`flex-1 py-2 text-xs font-medium ${activeView === 'majorRequirements' ? 'border-b-2 border-blue-500 text-blue-600' : 'text-gray-500'}`}
+                >Requirements</button>
             </div>
 
             {activeView === 'courseSearch' && 
@@ -35,10 +40,18 @@ function LeftInfoPanel({newCourses, allNodes}: any){
 
             {activeView === 'majorRequirements' && 
             <MajorRequirementsPanel 
-                majorData={majorData}
+                activeMajor={activeMajor}
                 allNodes={allNodes}
                 newCourses={newCourses}
                 onDragStart={onDragStart}
+            />}
+
+            {activeView === 'degreePanel' && 
+            <DegreeCreatorPanel
+                allPrograms={allPrograms} 
+                selectedProgram={selectedLabel} 
+                setSelectedProgram={setSelectedLabel} 
+                isLoading={isLoading}
             />}
 
         </div>
